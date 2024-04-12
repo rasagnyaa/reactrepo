ff8b7c26a01265b853858cd1bd1c8fc3b00d809b
rasagnyak <rasagnyakannuri@gmail.com>
2024-04-12 18:24:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import CustomSpinner from './spinner/spinner';
 import ButtonComponent from './subscribe/button';
 
 function App() {
   return (
     <div className="App">
      <ButtonComponent></ButtonComponent>
+     <CustomSpinner></CustomSpinner>
     </div>
   );
 }
