3344c183a37525049f2432ee188106ca3a792942
rasagnyak <rasagnyakannuri@gmail.com>
2024-04-13 12:57:43 +0530

    done

--- src/Life Cycle/Mounting/productlisting.js
+++ src/Life Cycle/Mounting/productlisting.js
@@ -19,6 +19,15 @@ this.setState({
     products:result.data.products
 })
 }
+deleteItem=(index)=>{
+const newProducts=this.state.products.filter((eachObject,i)=>{
+    return i!==index
+})
+this.setState({
+    products:newProducts
+})
+
+}
 render(){
     return(
         <>
@@ -29,15 +38,17 @@ render(){
             <div className="productsList">
                 <>
                 {
-                    this.state.products.map(eachObject=>{
+                    this.state.products.map((eachObject,index)=>{
                         const{description,title,thumbnail,price}=eachObject
                         return(
                             <>
                             <div className="cards">
-                                <h3>{title}</h3>
-                                <h4>{description}</h4>
-                                <img className="thumbnail"src={thumbnail} alt="title"width={200} height={200}/>
+                            <img className="thumbnail"src={thumbnail} alt="title"width={200} height={200}/>
+                                <h3 className="title">{title}</h3>
+                                <h4 className="description">{description}</h4>
+                                
                                 <h5 className="price">₹{price}</h5>
+                                <button className="dltbtn"onClick={()=>this.deleteItem(index)}>Delete</button>
                             </div>
 
 
