18e001edcfa08b60c9d66b9b01e9cd7bf78149b6
rasagnyaa <rasagnyakannuri@gmail.com>
2024-06-22 22:29:45 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
-import ButtonComponent from './Components/functionalComponent/button/button';
-import Form from './Components/functionalComponent/form/form';
-import ImageComponent from './Components/functionalComponent/image/image';
-import { ListParentOrderd, ListParentUnorderd } from './Components/functionalComponent/list/listparent';
-import Table from './Components/functionalComponent/table/table';
+import ListMap from './Components/functionalComponent/12-task/listmap/listmap';
+// import ButtonComponent from './Components/functionalComponent/button/button';
+// import Form from './Components/functionalComponent/form/form';
+// import ImageComponent from './Components/functionalComponent/image/image';
+// import { ListParentOrderd, ListParentUnorderd } from './Components/functionalComponent/list/listparent';
+// import Table from './Components/functionalComponent/table/table';
 
 function App() {
   return (
     <div className="App">
-     <Form></Form>
+     {/* <Form></Form>
     <Table></Table>
     <ListParentOrderd></ListParentOrderd>
     <ListParentUnorderd></ListParentUnorderd>
     <ButtonComponent></ButtonComponent>
-    <ImageComponent></ImageComponent>
+    <ImageComponent></ImageComponent> */}
+    <ListMap></ListMap>
     </div>
   );
 }
