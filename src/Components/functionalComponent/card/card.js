0f11fa353838caa2489a6e84cfd041637708782d
rasagnyaa <rasagnyakannuri@gmail.com>
2024-06-23 12:12:39 +0530

    commit

--- src/Components/functionalComponent/card/card.js
+++ src/Components/functionalComponent/card/card.js
@@ -1,10 +1,12 @@
 import ImageComponent from "../image/image"
 import ButtonComponent from "../button/button"
+import ListMap from "../listmap/list"
 const Card=()=>{
     return(
         <>
        <ImageComponent/> 
        <ButtonComponent/>
+       <ListMap/>
        </>
     )
 }
