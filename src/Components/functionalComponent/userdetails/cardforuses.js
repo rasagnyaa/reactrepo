fd0652d918fa8a80e13b369dc5a621760afab623
rasagnyaa <rasagnyakannuri@gmail.com>
2024-07-21 13:48:02 +0530

    user-details

--- src/Components/functionalComponent/userdetails/cardforuses.js
+++ src/Components/functionalComponent/userdetails/cardforuses.js
@@ -1,36 +1,33 @@
+const CardforUsers = ({ details }) => {
+  const cardStyle = {
+    border: "1px solid lightgray",
+    height: 350,
+    width: 400,
+    display: "inline-block",
+    textAlign: "center",
+    margin: "0px 40px 10px ", // Horizontal spacing of 20px
+    borderRadius: "2rem",
+    boxShadow: "10px 10px 10px 5px lightgray",
+    backgroundColor: "gray",
+  };
+  return (
+    <>
+      <div style={cardStyle}>
+        <h3>Username:{details.username}</h3>
+        <p>Name:{details.name}</p>
+        <p>Email:{details.email}</p>
 
+        <p>
+          Comapany:{details.company.name}.{details.address.street}
+        </p>
+        <h3>Address:</h3>
+        <p>Suite:{details.address.suite}</p>
+        <p>City:{details.address.city}</p>
 
-const CardforUsers=({details})=>{
-const cardStyle={
-border:"1px solid lightgray",
-height:350,
-width:400,
-display:"inline-block",
-textAlign:"center",
-margin: "0px 40px 10px ", // Horizontal spacing of 20px
-borderRadius:"2rem",
-boxShadow:"10px 10px 10px 5px lightgray",
-backgroundColor:"gray"
-}
-return <>
-
-<div style={cardStyle}>
-<h3>Username:{details.username}</h3>
-<p>Name:{details.name}</p>
-<p>Email:{details.email}</p>
-
-<p>Comapany:{details.company.name}.{details.address.street}</p>
-    <h3>Address:</h3><p>Suite:{details.address.suite}</p>
-    <p>City:{details.address.city}</p>
-    
-    <p>Zipcode:{details.address.zipcode}</p>
-    <p>phone:{details.phone}</p>
-
-</div>
-
-
-</>
-
-
-}
-export default CardforUsers
+        <p>Zipcode:{details.address.zipcode}</p>
+        <p>phone:{details.phone}</p>
+      </div>
+    </>
+  );
+};
+export default CardforUsers;
--- src/Components/functionalComponent/userdetails/user-deatils.js
+++ src/Components/functionalComponent/userdetails/user-deatils.js
@@ -3,50 +3,44 @@ import { useEffect, useState } from "react";
 import CardforUsers from "./cardforuses";
 import CustomSpinnner2 from "../spinner/spinner";
 
-const UserDetails=()=>{
-const[data,setData]=useState([])
-const[loader,setLoader]=useState(true)
+const UserDetails = () => {
+  const [data, setData] = useState([]);
+  const [loader, setLoader] = useState(true);
 
-useEffect(()=>{
-    fetchData()
-},[])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-const fetchData=async()=>{
-try{
-    const response=await axios.get("https://jsonplaceholder.typicode.com/users")
-    
-    console.log(response,"response")
-    setData(response.data)
-}catch(error){
-    console.log(error,"error")
-}finally{
-    setLoader(false)
-}
-}
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
 
+      console.log(response, "response");
+      setData(response.data);
+    } catch (error) {
+      console.log(error, "error");
+    } finally {
+      setLoader(false);
+    }
+  };
 
-return(
-   <center>
-
-<h1 style={{fontFamily:"sans-serif"}}>Alumnus of Law College</h1>
-{loader?(<CustomSpinnner2/>):(data.map((eachUser )=>{
-return (
-    <>
-<CardforUsers key={eachUser.id}  details={eachUser}/>
-    
-    
-    </>
-    
-    
-    
-
-
-    
-)
-}))
-
-}
-   </center>
-)
-}
-export default UserDetails
+  return (
+    <center>
+      <h1 style={{ fontFamily: "sans-serif" }}>Alumnus of Law College</h1>
+      {loader ? (
+        <CustomSpinnner2 />
+      ) : (
+        data.map((eachUser) => {
+          return (
+            <>
+              <CardforUsers key={eachUser.id} details={eachUser} />
+            </>
+          );
+        })
+      )}
+    </center>
+  );
+};
+export default UserDetails;
